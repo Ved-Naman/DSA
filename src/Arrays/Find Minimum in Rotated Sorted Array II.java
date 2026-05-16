d70cbfb0eeedcde015483f2cb987626d66c2b1fa
Ved-Naman <vednamanofficial@gmail.com>
2026-05-16 19:13:07 +0530

    new commit

--- src/Arrays/Find Minimum in Rotated Sorted Array II.java
+++ src/Arrays/Find Minimum in Rotated Sorted Array II.java
@@ -4,3 +4,27 @@ class Solution {
         return nums[0];
     }
 }
+
+---------------------------------------------------------------------
+
+
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else if (nums[mid] < nums[high]) {
+                high = mid;
+            } else {
+                high--;
+            }
+        }
+
+        return nums[low];
+    }
+}
