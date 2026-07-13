b8fe66b15181040abb9195110ad9f4c08e6ae1b2
Ved-Naman <vednamanofficial@gmail.com>
2026-07-13 18:59:51 +0530

    new commit

--- src/Hash Table/Unique Number of Occurrences.java
+++ src/Hash Table/Unique Number of Occurrences.java
@@ -13,3 +13,30 @@ class Solution {
         return true;
     }
 }
+
+
+------------------------------------------------------------------------------------------------------
+
+
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> counts = new HashMap<>();
+
+        for (int num : arr) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        ArrayList<Integer> freqList = new ArrayList<>(counts.values());
+
+        for (int i = 0; i < freqList.size(); i++) {
+            for (int j = i + 1; j < freqList.size(); j++) {
+                if (freqList.get(i).equals(freqList.get(j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
