04d26fb8a324b4e05ff61cd6f7914c231b9f5ff4
Ved-Naman <vednamanofficial@gmail.com>
2026-07-13 12:18:06 +0530

    new commit

--- src/Arrays/Move Zeroes.java
+++ src/Arrays/Move Zeroes.java
@@ -15,3 +15,26 @@ class Solution {
         }
     }
 }
+
+
+---------------------------------------------------------------------------
+
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] x = new int[nums.length];
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                x[index] = nums[i];
+                index++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = x[i];
+        }
+    }
+}
