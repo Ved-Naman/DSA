67d2eb6256320bbe4b0e42bda8190a16837f6b99
Ved-Naman <vednamanofficial@gmail.com>
2026-08-21 04:59:29 +0530

    new commit

---
+++ src/Backtracking/Combination Sum II.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] candidates, int remain, int start) {
+        if (remain == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > remain) {
+                break;
+            }
+
+            current.add(candidates[i]);
+            backtrack(result, current, candidates, remain - candidates[i], i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
