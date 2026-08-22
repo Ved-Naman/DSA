cce9d106f919ab39098da8863fd08b3c20df872d
Ved-Naman <vednamanofficial@gmail.com>
2026-08-23 03:14:50 +0530

    new commit

--- src/Arrays/Squares of a Sorted Array.java
+++ src/Arrays/Squares of a Sorted Array.java
@@ -8,3 +8,16 @@ class Solution {
         return n;
     }
 }
+
+-----------------------------------------------------------------
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i< nums.length; i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
